ebe25007436044e2da3032be6f6b028f35d2e49a
raquelpachy <raquelpachicano38@gmail.com>
2025-12-31 22:58:35 -0600

    Create raquelpachy.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/raquelpachy.py
@@ -0,0 +1,26 @@
+#https://python.org
+"""
+este es un comentario
+de varias 
+lineas 
+"""
+'''
+este tambien 
+es un comentario de varias 
+lineas
+'''
+var_4 = "esta es mi variable"
+var_4 = "esta es mi nueva variable"
+
+CONSTANTE = "esta es mi constante" # por convenccion 
+
+# tipos de datos primitivos 
+my_variable = 7777
+my_float = 3.1416
+my_bool = True 
+my_bool = False
+my_string = "cadena de texto"
+
+print("¡Hola, python!")
+
+
